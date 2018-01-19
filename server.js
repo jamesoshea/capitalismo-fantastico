a897adca989e1f937b881f35a8f7341bfb510c05
James O'Shea <jamesoshea89@hotmail.com>
2018-01-19 19:49:38 +0100

    promises returning multiple queries nicely, probably

--- server.js
+++ server.js
@@ -1,6 +1,56 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
 const app = express()
+
+app.use(bodyParser.json())
+
+const baseEndpoint = 'https://api.github.com' 
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('/user/:username', (req, res) => {
+	const stats = []
+	stats.push(
+		axios.get(`${baseEndpoint}/users/${req.params.username}`)
+			.then(response => {
+				const userInfo = processUser(response.data)
+				return userInfo
+			})
+			.catch((err) => {
+				console.error(err)
+			})
+	)
+	stats.push(
+		axios.get(`${baseEndpoint}/users/${req.params.username}/followers`)
+			.then(response => {
+				const followerInfo = processFollowers(response.data)
+				return followerInfo
+			})
+			.catch((err) => {
+				console.error(err)
+			})
+	)
+	Promise.all(stats)
+		.then(data => {
+			res.json(data)
+		})
+})
+
+app.listen(3000, () => {
+	console.log('Example app listening on port 3000!')
+})
+
+const processUser = (user) => {
+	if(user.hireable) {
+		return ({
+			bio: user.bio,
+			repoCount: user.public_repos,
+		})
+	}
+	return false
+}
+
+const processFollowers = (followers) => {
+		return followers.length
+}
