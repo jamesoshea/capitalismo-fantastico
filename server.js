7368f68e27829bbb56ad553aea18a00e556d80a9
James O'Shea <jamesoshea89@hotmail.com>
2018-01-19 21:03:10 +0100

    wow functions

--- server.js
+++ server.js
@@ -14,41 +14,47 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/user/:username', (req, res) => {
 	const stats = []
 	stats.push(
-		axios.get(`${baseEndpoint}/users/${req.params.username}`)
+		axios.get(`${baseEndpoint}/users/${req.params.username}?access_token=${secrets.gitHubKey}`)
 			.then(response => {
 				const userInfo = processUser(response.data)
 				return userInfo
 			})
 			.catch((err) => {
-				res.error(err)
+				console.log(err)
 			})
 	)
 
 	stats.push(
-		axios.get(`${baseEndpoint}/users/${req.params.username}/followers`)
+		axios.get(`${baseEndpoint}/users/${req.params.username}/followers?access_token=${secrets.gitHubKey}`)
 			.then(response => {
 				const followerInfo = processFollowers(response.data)
 				return followerInfo
 			})
 			.catch((err) => {
-				res.error(err)
+				res.send(err)
 			})
 	)
 
 	stats.push(
-		axios.get(`${baseEndpoint}/users/${req.params.username}/repos`)
+		axios.get(`${baseEndpoint}/users/${req.params.username}/repos?access_token=${secrets.gitHubKey}`)
 			.then(response => {
 				const repoInfo = processRepos(response.data)
 				return repoInfo
 			})
 			.catch((err) => {
-				res.error(err)
+				res.send(err)
 			})
 	)
 
 	Promise.all(stats)
 		.then(data => {
-			res.json(data)
+			const result = data.reduce((obj, stats) => {
+				return Object.assign(obj, stats)
+			}, {})
+			res.json(result)
+		})
+		.catch((err) => {
+			res.send(err)
 		})
 })
 
@@ -70,5 +76,8 @@ const processFollowers = (followers) => {
 }
 
 const processRepos = (repos) => {
-	return ({ repoCount: repos.length })
+	const repoScore = repos.reduce((accumulator, repo) => {
+		return accumulator + (repo.stargazers_count * 2) + (repo.watchers * 3) + (repo.forks * 10)
+	}, 0)
+	return ({ repoCount: repos.length, repoScore })
 }
